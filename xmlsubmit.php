776258b49e53645b6ca4d2f51948684e5f63af7b
TaXXoR <taxxor@trash-mail.com>
2014-10-22 13:57:56 +0200

    added comments

--- xmlsubmit.php
+++ xmlsubmit.php
@@ -6,10 +6,10 @@ $scores = new DOMDocument();
 $scores->load("highscore.xml");
 $scoresRoot = $scores->getElementsByTagName('highscore')->item(0);
 
-$scoresNode = $scores->createElement('player');
+$scoresNode = $scores->createElement('player');							// erstellt das komplexe Element 'player'
 $scoresRoot->appendChild($scoresNode);
-$scoresNode->appendChild($scores->createElement("name", $name));
-$scoresNode->appendChild($scores->createElement("score", $score));
+$scoresNode->appendChild($scores->createElement("name", $name));		// fügt dem Element 'player' das simple Element 'name' hinzu mit der Variable Name
+$scoresNode->appendChild($scores->createElement("score", $score));		// fügt dem Element 'player' das simple Element 'score' hinzu mit der Variable Score
 
 $scores->save("highscore.xml");
 
