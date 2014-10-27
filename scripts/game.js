ed655069c6178828acec24c268f03473a241bf87
Karsten Haertner <k.haertner@gmx.de>
2014-10-27 23:06:48 +0100

    added functions

--- scripts/game.js
+++ scripts/game.js
@@ -13,6 +13,78 @@ var ingo = false;
 var destroyed = 0;
 
 
+//canvas
+var mainCanvas = document.getElementById("myCanvas");
+var mainContext = mainCanvas.getContext('2d');
+
+var circles = new Array();
+ 
+var requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame ||
+                            window.msRequestAnimationFrame;
+
+function Circle(radius, speed, width, xPos, yPos) {
+    this.radius = radius;
+    this.speed = speed;
+    this.width = width;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.opacity = .05 + Math.random() * .5;
+
+    this.counter = 0;
+
+    var signHelper = Math.floor(Math.random() * 2);
+
+    if (signHelper == 1) {
+        this.sign = -1;
+    } else {
+        this.sign = 1;
+    }
+}
+
+Circle.prototype.update = function () {
+
+    this.counter += this.sign * this.speed;
+
+    mainContext.beginPath();
+     
+    mainContext.arc(this.xPos + Math.cos(this.counter / 100) * this.radius, 
+                    this.yPos + Math.sin(this.counter / 100) * this.radius, 
+                    this.width, 
+                    0, 
+                    Math.PI * 2,
+                    false);
+                     
+    mainContext.closePath();
+
+    mainContext.fillStyle = 'rgba(185, 211, 238,' + this.opacity + ')';
+    mainContext.fill();
+};
+
+function drawCircles() {
+    for (var i = 0; i < 100; i++) {
+        var randomX = Math.round(-200 + Math.random() * 700);
+        var randomY = Math.round(-200 + Math.random() * 700);
+        var speed = .2 + Math.random() * 3;
+        var size = 5 + Math.random() * 100;
+
+        var circle = new Circle(100, speed, size, randomX, randomY);
+        circles.push(circle);
+    }
+    draw();
+}
+drawCircles();
+
+function draw() {
+    mainContext.clearRect(0, 0, 500, 500);
+
+    for (var i = 0; i < circles.length; i++) {
+        var myCircle = circles[i];
+        myCircle.update();
+    }
+    requestAnimationFrame(draw);
+}
 
 /*$(document).keydown(function(e) { 				// noch nicht implementiert, aber evtl wollen wir tasten nutzen.
     switch(e.which) {
@@ -170,13 +242,16 @@ function createBox(id){							//Hindernis berechnen und als container anzeigen
 	}else if(powerUp<5){
 		box = $("<div id=box"+id+" class='box' style='width: 20px; height: 20px; cursor:none; background: url(./images/coin.png); position: fixed; border-radius: 10px; left: "+width+"px; top: "+y+"px' onmouseover='coins("+id+");'></div>");
 	}else if(ingo == false) {
-		$("canvas").drawArc({
-		  draggable: true,
-		  fillStyle: "green",
-		  x: cometheight, y: cometheight,
-		  radius: 50
-		});
-		// box = $("<canvas id=box"+id+" class='box bad' style='width:"+cometheight+"px; height:"+cometheight+"px; cursor:none; background: url(./images/comet_big.png); position: fixed; border-radius: 50px; left: "+width+"px; top: "+y+"px' onmouseover='lose("+id+", "+reason+");'></canvas>");
+
+		var randomX = 1900;
+        var randomY = Math.floor(Math.random() * 1080);
+        var speed = .2 + Math.random() * 3;
+        var size = cometheight/2;
+
+        var circle = new Circle(100, speed, size, randomX, randomY);
+        circles.push(circle);
+
+		//box = $("<canvas id=box"+id+" class='box bad' style='width:"+cometheight+"px; height:"+cometheight+"px; cursor:none; background: url(./images/comet_big.png); position: fixed; border-radius: 50px; left: "+width+"px; top: "+y+"px' onmouseover='lose("+id+", "+reason+");'></canvas>");
 	}else{
 		box = $("<div id=box"+id+" class='box bad' style='width: 20px; height: 20px; cursor:none; background: url(./images/ingo.png); position: fixed; border-radius: 10px; left: "+width+"px; top: "+y+"px' onmouseover='lose("+id+", "+reason+");'></div>");
 	};
